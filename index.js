7d689149fa8cb66f0cd1041505045025ada0b24e
tsigle <terrysigle@pingidentity.com>
2023-07-14 03:02:38 +0000

    update

--- index.js
+++ index.js
@@ -2,31 +2,33 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const lintFlow = require('./lintFlow');
 
-try {
-  // Get the inputs
-  const ignoreWarnings = core.getInput('ignore-warnings');
-  const flows = core.getInput('flows');
+(async () => {
+  try {
+    // Get the inputs
+    const ignoreWarnings = core.getInput('ignore-warnings');
+    const flows = core.getInput('flows');
 
-  // Debug
-  console.log(`Running with flows = `, flows);
-  console.log(`Running with ignoreWarnings = `, ignoreWarnings);
-  console.log("About to lint flow");
+    // Debug
+    console.log(`Running with flows = `, flows);
+    console.log(`Running with ignoreWarnings = `, ignoreWarnings);
+    console.log("About to lint flow");
 
-  const rawResults = lintFlow(false, flows);
+    const rawResults = await lintFlow(false, flows);
 
-  console.log(rawResults);
+    console.log(rawResults);
 
-  core.setOutput("pass", true);
-  core.setOutput("warnings", "Not Set");
-  core.setOutput("errors", "Not Set");
-  core.setOutput("raw-results", "Not Set");
+    core.setOutput("pass", true);
+    core.setOutput("warnings", "Not Set");
+    core.setOutput("errors", "Not Set");
+    core.setOutput("raw-results", rawResults);
 
 
-  // const time = (new Date()).toTimeString();
-  // core.setOutput("time", time);
-  // // Get the JSON webhook payload for the event that triggered the workflow
-  const context = JSON.stringify(github.context, undefined, 2)
-  console.log(`The event payload: ${context}`);
-} catch (error) {
-  core.setFailed(error.message);
-}
+    // const time = (new Date()).toTimeString();
+    // core.setOutput("time", time);
+    // // Get the JSON webhook payload for the event that triggered the workflow
+    const context = JSON.stringify(github.context, undefined, 2)
+    console.log(`The event payload: ${context}`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+})
